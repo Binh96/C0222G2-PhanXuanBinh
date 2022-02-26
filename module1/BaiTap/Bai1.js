f68ed2fd232e5808517e68ba88efedb68743aedd
Binh96 <phanxuanbinh96@gmail.com>
2022-02-26 17:11:47 +0700

    new files

---
+++ module1/BaiTap/Bai1.js
@@ -0,0 +1,12 @@
+
+
+function hinhChuNhat(m, n){
+    for(var i=0; i< n; i++){
+        for(var j=0; j<m; j++){
+            console.log("*");
+        }
+        console.log("\n");
+    }
+}
+
+console.log(hinhChuNhat(4,5));
