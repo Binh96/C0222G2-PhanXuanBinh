9b16987c6de17d3f95bd57277a27c72e34b3de94
Binh96 <phanxuanbinh96@gmail.com>
2022-03-31 16:50:23 +0700

    lear java

---
+++ module2/project_module2/src/advanced_programming_with_java/SystemTime.java
@@ -0,0 +1,11 @@
+package advanced_programming_with_java;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); // Lấy ra thời gian hiện tại
+
+        System.out.println("Now is: " + now);
+    }
+}
