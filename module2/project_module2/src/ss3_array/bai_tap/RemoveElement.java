c7a8053442ae3d06bd48bab71465593e5c7efec3
Binh96 <phanxuanbinh96@gmail.com>
2022-04-04 17:19:50 +0700

    update bai tap remove element

--- module2/project_module2/src/ss3_array/bai_tap/RemoveElement.java
+++ module2/project_module2/src/ss3_array/bai_tap/RemoveElement.java
@@ -26,27 +26,35 @@ public class RemoveElement {
         }
 
         System.out.print("Element of array: ");
-        for(int j=0; j< array.length; j++){
-            System.out.print(array[j]+ "\t");
+        for (int k : array) {
+            System.out.print(k + "\t");
         }
-
+            // Xóa phần tử
         System.out.print("\nEnter element you want to remove: ");
         int delElement = input.nextInt();
         int index = 0;
+        boolean check = false;
         for(int j = 0; j < array.length; j++){
             if(delElement == array[j]){
                 index = j;
+                check = true;
             }
         }
 
-        for(int j = index; j < array.length-1; j++){
-            array[j] = array[j + 1];
-        }
-        System.out.print("Array after remove element: ");
+        if(check){
+            for(int j = index; j < array.length-1; j++){
+                array[j] = array[j + 1];
+            }
+            System.out.print("Array after remove element: ");
 
-        for(int j= 0; j< array.length-1; j++){
-            System.out.print(array[j] + "\t");
+            for(int j= 0; j< array.length-1; j++){
+                System.out.print(array[j] + "\t");
+            }
         }
+        else{
+            System.out.print("Element doesn't exist");
+        }
+
     }
 
 }
