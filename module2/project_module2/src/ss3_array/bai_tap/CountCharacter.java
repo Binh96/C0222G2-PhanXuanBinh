5e3cf8a1fd2fb13d676ff44aa369c572debcac1b
Binh96 <phanxuanbinh96@gmail.com>
2022-04-04 16:29:49 +0700

    new update ss3

--- module2/project_module2/src/ss3_array/bai_tap/CountCharacter.java
+++ module2/project_module2/src/ss3_array/bai_tap/CountCharacter.java
@@ -1,8 +1,22 @@
 package ss3_array.bai_tap;
 
+import java.util.Scanner;
+
 public class CountCharacter {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         String strings = "Phan Xuan Binh";
-        System.out.print(strings.length());
+
+        System.out.print("Enter character you want to find: ");
+        char character= input.nextLine().charAt(0);
+        System.out.print("\ncharacter you want to find: "+ character);
+
+        int count=0;
+        for(int i=0; i< strings.length(); i++){
+            if(character == strings.charAt(i)){
+                count++;
+            }
+        }
+        System.out.print("\nTimes character has in string: "+ count);
     }
 }
