0213b25e31b6e5ec1e61b0473a4fc41ef559fe8f
Binh96 <phanxuanbinh96@gmail.com>
2022-05-26 07:44:11 +0700

    update early morning 26-05-2022

--- module2/project_module2/src/case_study/views/Demo.java
+++ module2/project_module2/src/case_study/views/Demo.java
@@ -1,27 +1,21 @@
 package case_study.views;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 
-import case_study.utils.valid.CheckBirthday;
 import case_study.utils.valid.DateException;
-import case_study.utils.valid.InputDate;
 
 import java.text.ParseException;
-import java.util.Date;
-import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) throws ParseException, DateException {
         Scanner scanner = new Scanner(System.in);
-
 //        Date dob = InputDate.dateValid();
 //        while(!CheckBirthday.checkBirthday(String.valueOf(dob))){
 //            dob = InputDate.dateValid();
 //        }
+
     }
+
 }
 
